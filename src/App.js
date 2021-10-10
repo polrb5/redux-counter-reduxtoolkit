176ac71d18e7fbb0bbe893e03629d6d743841fc4
Pol Reig <83955122+polrb5@users.noreply.github.com>
2021-10-10 12:19:26 +0200

    Redux counter with Redux Toolkit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment, reset } from "./redux/counter";
 
 function App() {
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="h-screen w-screen bg-purple-50 flex justify-center items-center">
+      <div className="h-40 w-40 text-center">
+        <span className="text-6xl">{count}</span>
+        <div className="flex justify-center items-center">
+          <button
+            onClick={() => dispatch(decrement())}
+            className="bg-gray-400 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-full m-1"
+          >
+            -
+          </button>
+
+          <button
+            onClick={() => dispatch(increment())}
+            className="bg-gray-400 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-full m-1"
+          >
+            +
+          </button>
+        </div>
+        <button
+          onClick={() => dispatch(reset())}
+          className="bg-gray-400 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded"
         >
-          Learn React
-        </a>
-      </header>
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
